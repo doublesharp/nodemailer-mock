00fcbffb1e4810a9ccfeda81bbb6d642319c5ae6
Justin Silver <jsilver@doublesharp.com>
2024-01-02 14:23:34 -0800

    hide dist

--- dist/lib/messages.d.ts
+++
@@ -1,6 +0,0 @@
-import { SentMessageInfo } from 'nodemailer';
-export declare const messages: {
-    readonly success_response: string;
-    readonly fail_response: Error;
-    readonly info: () => SentMessageInfo;
-};
