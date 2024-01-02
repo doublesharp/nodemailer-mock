e93857508fde2704e80f88a85a209892b8f510db
Justin Silver <jsilver@doublesharp.com>
2024-01-02 14:28:43 -0800

    cleanup

--- examples/__mocks__/nodemailer.js
+++ examples/__mocks__/nodemailer.js
@@ -1,10 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 'use strict';
 
-// const nodemailermock = require('../../dist/nodemailer-mock');
-const { getMockFor, nodemailerLoading } = require('../../dist/nodemailer-mock');
+const { getMockFor } = require('../../dist/nodemailer-mock');
 const nodemailer = require('nodemailer');
-// if (nodemailerLoading) {
 module.exports = getMockFor(nodemailer);
-// }
-// module.exports = nodemailer;
